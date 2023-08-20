afc6ddc6624d8d7300b050aad1bbb033921fc483
Karma-234 <busari1kamal@gmail.com>
2023-08-20 15:12:39 -0700

    Added custom button

---
+++ lib/shared_widgets/app_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppButton extends StatelessWidget {
+  final VoidCallback onPress;
+  final bool isLoading;
+  final String text;
+  const AppButton(
+      {super.key,
+      required this.onPress,
+      this.isLoading = false,
+      this.text = 'Continue'});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: isLoading ? null : onPress,
+      child: isLoading
+          ? const CupertinoActivityIndicator(
+              color: Colors.black,
+            )
+          : Text(
+              text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.white),
+            ),
+    );
+  }
+}
--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,8 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:portfolio/shared_widgets/app_button.dart';
 import 'package:portfolio/shared_widgets/input_field.dart';
 import 'package:portfolio/utils/extensions.dart';
+
+import 'package:portfolio/view_model/login/login.dart';
 
 @RoutePage()
 class LoginView extends StatelessWidget {
@@ -10,6 +14,7 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    late final state = LoginState();
     return Scaffold(
       body: SafeArea(
         minimum: EdgeInsets.all(16.r),
@@ -29,6 +34,11 @@ class LoginView extends StatelessWidget {
               hint: 'Enter password',
               header: 'Password',
             ),
+            Observer(builder: (context) {
+              return AppButton(
+                onPress: () {},
+              );
+            })
           ].separate(16.h),
         ),
       ),
