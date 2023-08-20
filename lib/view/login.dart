e487e508bc8183ae9d2956e4940a43e433e1d489
Karma-234 <busari1kamal@gmail.com>
2023-08-20 15:05:06 -0700

    WIP: Added form fields to login

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,5 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:portfolio/shared_widgets/input_field.dart';
+import 'package:portfolio/utils/extensions.dart';
 
 @RoutePage()
 class LoginView extends StatelessWidget {
@@ -7,12 +10,26 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
+        minimum: EdgeInsets.all(16.r),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+          children: [
+            const Text(
+              'Welcome',
+              textAlign: TextAlign.center,
+            ),
+            const AppInputField(
+              hint: 'Enter email',
+              header: 'Email',
+            ),
+            const AppInputField(
+              hint: 'Enter password',
+              header: 'Password',
+            ),
+          ].separate(16.h),
         ),
       ),
     );
